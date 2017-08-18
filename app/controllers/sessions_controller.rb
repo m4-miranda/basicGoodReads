1b6482ab29017f68aa791cda71affb77913735eb
Marc Miranda <marcmiranda1995@gmail.com>
2017-08-18 10:53:15 +0530

    add flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      flash.now[:success] = "Successfully signed in"
       log_in user
       redirect_to user
     else
+      flash.now[:danger] = "Incorrect password/username"
       render 'new'
     end
   end
