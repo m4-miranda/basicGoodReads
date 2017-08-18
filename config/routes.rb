622e6c51b88d329ecfbe04f980993b7a785a767a
Marc Miranda <marcmiranda1995@gmail.com>
2017-08-18 10:33:45 +0530

    Sessions controller and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'user/home'
 
-  get 'user/favorites'
-
-  get 'user/read'
-
-  get 'user/toread'
-
-  get 'user/reading'
+  get 'users/home'
+  
+  get '/login',   to: 'sessions#new'
+  post '/login',  to: 'sessions#create'
+  get '/logout',  to: 'sessions#delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
