dd24e1c4a3537f9f5889630ac39c1cdab16dff8c
Marc Miranda <marcmiranda1995@gmail.com>
2017-08-22 04:02:00 +0530

    Refactored routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   post '/login',  to: 'sessions#create'
   delete '/logout',  to: 'sessions#delete'
 
-  post '/search', to: 'users#search'
+  post '/users', to: 'users#search'
+  get '/users',  to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 end
