e4913846675bda606a51dc52aab591778ec5d8a9
Marc Miranda <marcmiranda1995@gmail.com>
2017-08-22 14:08:41 +0530

    Moved lists view to books controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,18 +2,7 @@ class ListsController < ApplicationController
   def new
   end
 
-  def show
-    @user = current_user
-    @desired_list = @user.lists.find(params[:list_id])
-    @list_of_books = @desired_list.books
-  end
 
-  def add
-    @user = current_user
-    @user.lists.find(params[:list_id]).books.create( title: params[:lists][:book_title],
-                                      api_id: params[:lists][:book_id],
-                                      image_link: params[:lists][:book_img] )
-    flash.now[:success] = "Book added to favorites"
-    redirect_to home_path
-  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   get 'lists/new'
 
-  get '/lists', to: 'lists#show'
-
   root 'sessions#new'
 
   get '/login',   to: 'sessions#new'
   post '/login',  to: 'sessions#create'
   delete '/logout',  to: 'sessions#delete'
 
-  post '/lists', to: 'lists#add'
-
+  post '/books', to: 'books#add'
+  delete '/books', to: 'books#delete'
+  get '/books', to:'books#show'
+  
   post '/home', to: 'books#index'
   get '/home',  to: 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
