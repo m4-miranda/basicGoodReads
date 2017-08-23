029fc16fefb558a1510e61bc73d39bc79954e88d
Marc Miranda <marcmiranda1995@gmail.com>
2017-08-23 12:49:24 +0530

    Create and delete lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,9 +9,20 @@ class ListsController < ApplicationController
   end
 
   def create
-    
+    if logged_in?
+      @user = current_user
+      new_list = @user.lists.create(name: params[:lists][:name], list_id: @user.lists.count + 1 )
+      redirect_to books_path(list_id: new_list.list_id)
+    end
   end
 
+  def delete
+    if logged_in?
+      @user = current_user
+      @user.lists.find_by(list_id: params[:list_id]).destroy
+      redirect_to settings_path
+    end
+  end
 
 
 end
